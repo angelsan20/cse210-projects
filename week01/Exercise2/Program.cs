ab409108a11d6aea3b6a612f7ba9616a406b6843
angelsan20 <angelasdrubal120@gmail.com>
2026-09-05 03:41:48 -0400

    Exercise 1 and Exercise 2 completed, the excercise 2 just complete the core requirements, I will be trying the stretch challenge.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello Team! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int percentage = int.Parse(userAnswer);
+
+        string gradeLetter = "";
+
+        if (percentage >= 90)
+        {
+           gradeLetter= "A";
+        }
+        else if (percentage >= 80)
+        {
+            gradeLetter ="B";
+        }
+        else if (percentage >= 70)
+        {
+            gradeLetter ="C";
+        }
+        else if (percentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't reach your goal this time, but with hard work, you'll get there. ");
+        }
     }
 }
