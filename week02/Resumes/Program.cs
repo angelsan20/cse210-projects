35c0678c692cee79a504984af56d011af3f49a0e
angelsan20 <angelasdrubal120@gmail.com>
2026-09-16 17:48:39 -0400

    Resumes project completed and functioning

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine("Hello! This is the Angel's Resumes Project.");
+        
+        Console.Write("");
+
+        Job jobOne = new Job();
+        jobOne._jobTitle = "Software Engineer";
+        jobOne._company = "Microsoft";
+        jobOne._startYear = 2019;
+        jobOne._endYear = 2022;
+
+        Job jobTwo = new Job();
+        jobTwo._jobTitle = "Manager";
+        jobTwo._company = "Apple";
+        jobTwo._startYear = 2022;
+        jobTwo._endYear = 2023;
+
+        Resume theResume = new Resume();
+        theResume._name = "Allison Rose";
+
+        theResume._jobs.Add(jobOne);
+        theResume._jobs.Add(jobTwo);
+
+        theResume.Display();
     }
 }
